7721bc71f7e63eb5f2d5071d588c83345befb211
elcomendante <karol@finsane.com>
2022-12-06 20:16:55 +0000

    test base

---
+++ test/Protocol-test.ts
@@ -0,0 +1,41 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { formatEther, parseEther, parseUnits } from "ethers/lib/utils";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Protocol, EMBToken, ERC20 } from "../typechain-types";
+
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("MultiSigWallet", () => {
+  const provider = ethers.provider;
+  let owner: SignerWithAddress;
+  let account1: SignerWithAddress;
+  let account2: SignerWithAddress;
+  let rest: SignerWithAddress[];
+
+  let EMBToken: EMBToken;
+  let protocol: Protocol;
+  let merkleRoot: string;
+  beforeEach(async function () {
+    [owner, account1, account2, ...rest] = await ethers.getSigners();
+
+    EMBToken = (await (
+      await ethers.getContractFactory("EMBToken")
+    ).deploy("EMB Token", "EMB")) as EMBToken;
+    await EMBToken.deployed();
+  });
+
+  beforeEach(async function () {
+    protocol = await (
+      await ethers.getContractFactory("Protocol")
+    ).deploy(account1.address, EMBToken.address);
+    await protocol.deployed();
+  });
+
+  describe("setup", async function () {
+    it("should deploy correctly", async () => {});
+  });
+  describe("Signature minting", async function () {
+    it("TODO", async () => {});
+  });
+});
--- test/protocol.spec.ts
+++ test/protocol.spec.ts
@@ -1,39 +1,39 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
-import { ethers } from "hardhat";
-import { Protocol, ERC20, EMBToken } from "../typechain-types";
+// import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+// import { expect } from "chai";
+// import { ethers } from "hardhat";
+// import { Protocol, ERC20, EMBToken } from "../typechain-types";
 
-const provider = ethers.provider
-let account1: SignerWithAddress
-let account2: SignerWithAddress
-let rest: SignerWithAddress[]
+// const provider = ethers.provider
+// let account1: SignerWithAddress
+// let account2: SignerWithAddress
+// let rest: SignerWithAddress[]
 
-let EMBToken: EMBToken
-let protocol: Protocol
-let merkleRoot: string
+// let EMBToken: EMBToken
+// let protocol: Protocol
+// let merkleRoot: string
 
-describe("Protocol", function () {
-  before(async () => {
-    [account1, account2, ...rest] = await ethers.getSigners();
+// describe("Protocol", function () {
+//   before(async () => {
+//     [account1, account2, ...rest] = await ethers.getSigners();
 
-    EMBToken = (await (await ethers.getContractFactory("EMBToken")).deploy("EMB Token", "EMB")) as EMBToken
-    await EMBToken.deployed()
-  });
+//     EMBToken = (await (await ethers.getContractFactory("EMBToken")).deploy("EMB Token", "EMB")) as EMBToken
+//     await EMBToken.deployed()
+//   });
 
-  beforeEach(async () => {
-    protocol = await (await ethers.getContractFactory("Protocol")).deploy(account1.address, EMBToken.address)
-    await protocol.deployed()
-  });
+//   beforeEach(async () => {
+//     protocol = await (await ethers.getContractFactory("Protocol")).deploy(account1.address, EMBToken.address)
+//     await protocol.deployed()
+//   });
 
-  describe("setup", () => {
+//   describe("setup", () => {
 
-    it("should deploy correctly", async () => {
-      // if the beforeEach succeeded, then this succeeds
-    });
+//     it("should deploy correctly", async () => {
+//       // if the beforeEach succeeded, then this succeeds
+//     });
 
-  describe("Signature minting", () => {
-    it ("TODO", async () => {
-      throw new Error("TODO: add more tests here!")
-    });
-  });
-});
+//   describe("Signature minting", () => {
+//     it ("TODO", async () => {
+//       throw new Error("TODO: add more tests here!")
+//     });
+//   });
+// });
