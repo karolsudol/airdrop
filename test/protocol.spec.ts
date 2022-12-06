8e2cb9700a8071b34e52b910adbb74ab53d68333
elcomendante <karol@finsane.com>
2022-12-06 17:02:38 +0000

    moved test

--- test/protocol.spec.ts
+++ test/protocol.spec.ts
@@ -14,26 +14,26 @@ let merkleRoot: string
 
 describe("Protocol", function () {
   before(async () => {
-    ;[account1, account2, ...rest] = await ethers.getSigners()
+    [account1, account2, ...rest] = await ethers.getSigners();
 
     EMBToken = (await (await ethers.getContractFactory("EMBToken")).deploy("EMB Token", "EMB")) as EMBToken
     await EMBToken.deployed()
-  })
+  });
 
   beforeEach(async () => {
     protocol = await (await ethers.getContractFactory("Protocol")).deploy(account1.address, EMBToken.address)
     await protocol.deployed()
-  })
+  });
 
   describe("setup", () => {
 
     it("should deploy correctly", async () => {
       // if the beforeEach succeeded, then this succeeds
-    })
+    });
 
   describe("Signature minting", () => {
     it ("TODO", async () => {
       throw new Error("TODO: add more tests here!")
-    })
-  })
+    });
+  });
 });
